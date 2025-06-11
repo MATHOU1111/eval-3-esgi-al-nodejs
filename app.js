f16672839ba7b84147895536619f147fcf20d3fd
Mathis <mathisdumage0@gmail.com>
2025-06-11 14:26:12 +0200

    projet fonctionne

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authRoute =require("./route/auth.route.js");
 const userRoute =require("./route/user.route.js");
 const {connect} = require('./model/connexion.js');
-const {sync} = require('./model/sync.js');
+const sync = require('./model/sync.js');
 const app = express();
 
 const database = async () => {
